86377777547600427415046b3921c9633589421a
AnLong98 <pedjaglavas98@gmail.com>
2021-11-21 20:13:04 +0100

    Initiated project and added Ocelot rerouting for paypal microservice

---
+++ Paypal.API/Controllers/PaymentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paypal.API.Controllers
+{
+    [Route("api/payments")]
+    public class PaymentController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Pay()
+        {
+            return Ok("Platio si baki, sve je ok");
+        }
+
+        [HttpGet("all")]
+        public IActionResult Get()
+        {
+            return Ok("Sve kafane zatvorio Lola, od Ilidze pa do Marindvora.");
+        }
+
+    }
+}
