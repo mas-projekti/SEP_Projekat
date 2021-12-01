fe23b7139063b17adf550a60c4727ecdb436b30f
lemara98 <mile.knezevic98@gmail.com>
2021-12-01 23:52:51 +0100

    Made ItemPage look good

--- WebShop/WebShop.Frontend/Frontend/src/pages/ItemPage.js
+++ WebShop/WebShop.Frontend/Frontend/src/pages/ItemPage.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Nav } from 'react-bootstrap';
 import { useParams } from 'react-router'
 // import MainBar from '../components/MainBar';
 
 const ItemPage = () => {
 
-    const params = useParams();
+    const [quantity, setQuantity] = useState(1); // '' is the initial state value
+
+    const item = {
+        id: 3981239812,
+        name: 'Sava Sumanovic - Šid',
+        placeHolderImg: "https://mediasfera.rs/wp-content/uploads/2019/01/sava-2-1200x894.jpg",
+        cost: 15,
+        amount: 80,
+        description: "This is the best item you can find on the market, eventualy you can find it on buvljak on Klisa, Novi Sad :).",
+    }
+    
+    const params = useParams()
+
+    // Fetch Item by id
+    // const fetchItem = async (id) => {
+    //     const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    //     const data = await res.json()
+    //     return data
+    // }
+
+    // Fetch User by userId in Item 
+    // const fetchUser = async (userId) => {
+    //     const res = await fetch(`http://localhost:5000/tasks/${userId}`)
+    //     const data = await res.json()
+    //     return data
+    // }
+
+    const user = {
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Michael_Jordan_crying.jpg/220px-Michael_Jordan_crying.jpg',
+        name: 'Dzordan Dzordanic',
+        email: 'michaeljordan@email.com',
+        phoneNumber: '+381623456789'
+    }
 
     return (
-        // <div>
-        //     {/* <MainBar/> */}
-        //     <div>
-        //         Hello
-        //     </div>
-        //     <h1>Item ID: {params.itemId}</h1>
-        // </div>
 
         <div className="page-div page" style={{backgroundColor:'black'}}>
             {/* <MainBar/> */}
-            <h1>ITEM NAME - Item ID : {params.itemId}</h1>
-            {/* <div className="row center" style={{width:'100%'}}>
+            {/* <div className="row center my-2" style={{width:'100%'}}>
                 <div className="col-3" />
                 <Nav className="col" style={{alignItems:'center'}}>
                     <Nav.Link href="#query=?">New Items</Nav.Link>
@@ -28,27 +53,52 @@ const ItemPage = () => {
                 </Nav>
                 <div className="col-3" />
             </div> */}
-            <div className="container">
+            <div className="container my-3">
                 <div className="row">
                     <div className="col"/>
                     <div className="col-10">
-                        <div className="row" style={{height:'300px'}}>
-                            <div className="col-4" style={{backgroundColor:'yellow'}}>
-                                <h3>Item picture</h3>
+                        <div className="row" style={{border:'1px solid white', borderRadius:'10px',  overflow: 'hidden', textOverflow: 'ellipsis'}}>
+                            <div className="col-4">
+                                <img 
+                                    src={item.placeHolderImg} 
+                                    alt="itemImage" 
+                                    style={{height:'90%', width:'90%', margin:'5%', borderRadius:'10px'}} />
                             </div>
-                            <div className="col-6" style={{backgroundColor:'green'}}>
-                                <h3>Item description</h3>
+                            <div className="col-4">
+                                <div className="row m-1">
+                                    <div >
+                                        <h3>{item.name}</h3>
+                                        <p>Cost per item: <h5>${item.cost}</h5></p>
+                                        <p>Amount available: <h5>{item.amount}</h5></p>
+                                    </div>
+                                    <div >
+                                        <p style={{border:'1px solid white', borderRadius:'10px', padding:'1px'}}>Description:<p>{item.description}</p></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="col-2" style={{backgroundColor:'purple'}}>
+                            <div className="col-4" style={{borderLeft: '1px dashed gray'}}>
                                 <h3>Profile Info</h3>
+                                <img 
+                                    src={user.imgSrc} 
+                                    alt="itemImage" 
+                                    style={{height:'50%', width:'50%', margin:'5%', borderRadius:'10px'}} />
+                                <p>Name: {user.name}</p>
+                                <p>Email: {user.email}</p>
+                                <p>Phone number: {user.phoneNumber}</p>
                             </div>
                         </div>
-                        <div className="row" style={{backgroundColor:'brown'}}>
-                            <h3>Button?</h3>
+                        <div className="row">
+                            <nav className="my-2">
+                                <p>
+                                    Wanted Amount: <input className="mx-2" type="number" min={1} max={item.amount} value={quantity} onChange={e => setQuantity(e.target.value)} style={{width:'100px'}}/>
+                                    Total Cost : <input className="mx-2" type="string" value={`$${quantity*item.cost}`} readonly style={{width:'100px'}}/>
+                                </p>
+                                <button type="button" className="btn btn-outline-light">BUY</button>
+                            </nav>
                         </div>
-                        <div className="row" style={{backgroundColor:'red'}}>
+                        {/* <div className="row">
                             <h3>Comment section</h3>
-                        </div>
+                        </div> */}
                     </div>
                     <div className="col"/>
                 </div>
