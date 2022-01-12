0cd326ad7d9e43afa4fd4c7163d3a8d49290da44
AnLong98 <pedjaglavas98@gmail.com>
2022-01-12 21:51:24 +0100

    added migration

---
+++ PSP.API/Migrations/20220109152614_FixedDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PSP.API.Migrations
+{
+    public partial class FixedDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PspClientId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_PspClientId",
+                table: "Transactions",
+                column: "PspClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_PspClients_PspClientId",
+                table: "Transactions",
+                column: "PspClientId",
+                principalTable: "PspClients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_PspClients_PspClientId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_PspClientId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "PspClientId",
+                table: "Transactions");
+        }
+    }
+}
---
+++ PSP.API/Migrations/20220109203603_AddedValidatingSecret.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PSP.API.Migrations
+{
+    public partial class AddedValidatingSecret : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ValidatingSecret",
+                table: "PspClients",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValidatingSecret",
+                table: "PspClients");
+        }
+    }
+}
