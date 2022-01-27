1093981fdf12cda08b0d481f88aa733174a61aed
AnLong98 <pedjaglavas98@gmail.com>
2022-01-27 18:45:36 +0100

    Added migrations to src control

---
+++ PSP.API/Migrations/20220122100546_BankTransaction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PSP.API.Migrations
+{
+    public partial class BankTransaction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankTransaction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MerchantID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MerchantPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    MerchantOrderID = table.Column<int>(type: "int", nullable: false),
+                    MerchantTimestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankTransaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankTransaction_Transactions_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "Transactions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransaction_TransactionId",
+                table: "BankTransaction",
+                column: "TransactionId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankTransaction");
+        }
+    }
+}
---
+++ PSP.API/Migrations/20220122134142_BankTransactionBankURL.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PSP.API.Migrations
+{
+    public partial class BankTransactionBankURL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BankTransaction_Transactions_TransactionId",
+                table: "BankTransaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BankTransaction",
+                table: "BankTransaction");
+
+            migrationBuilder.RenameTable(
+                name: "BankTransaction",
+                newName: "BankTransactions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BankTransaction_TransactionId",
+                table: "BankTransactions",
+                newName: "IX_BankTransactions_TransactionId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankURL",
+                table: "BankTransactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BankTransactions",
+                table: "BankTransactions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BankTransactions_Transactions_TransactionId",
+                table: "BankTransactions",
+                column: "TransactionId",
+                principalTable: "Transactions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BankTransactions_Transactions_TransactionId",
+                table: "BankTransactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BankTransactions",
+                table: "BankTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "BankURL",
+                table: "BankTransactions");
+
+            migrationBuilder.RenameTable(
+                name: "BankTransactions",
+                newName: "BankTransaction");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BankTransactions_TransactionId",
+                table: "BankTransaction",
+                newName: "IX_BankTransaction_TransactionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BankTransaction",
+                table: "BankTransaction",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BankTransaction_Transactions_TransactionId",
+                table: "BankTransaction",
+                column: "TransactionId",
+                principalTable: "Transactions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
