bb2272d358fbbf1d3895647fef6f633a726db8d1
AnLong98 <pedjaglavas98@gmail.com>
2022-01-27 18:41:46 +0100

    deleted useless stuff

---
+++ psp-web-app/src/views/transaction-outcomes/transaction-passed.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import  {useState, useEffect} from 'react';
+import { useParams , useNavigate} from 'react-router'
+
+export default function TransactionPassed() {
+    const routeParams = useParams();
+
+}
